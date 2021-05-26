7bf1820940303e7168f28c38a577ec58cc959977
Carmen Rodríguez García <80898726+Carmen-r@users.noreply.github.com>
2021-05-26 15:55:22 +0200

    Delete scr directory

--- scr/driver.py
+++
@@ -1,9 +0,0 @@
-
-from selenium import webdriver
-import os
-
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
-#driver = webdriver.Chrome(executable_path="../chromedriver", chrome_options = options)
-path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "chromedriver")
-driver = webdriver.Chrome(executable_path=path, chrome_options = options)
